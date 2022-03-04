2bd8365adee036ee963a09664f3257dcbf664b60
Martin Langenbacher <martin.langenbacher@telekom.de>
2022-03-04 13:55:59 +0100

    Some new files...

--- training_martin/uebungen5/kaukugel.js
+++ training_martin/uebungen5/kaukugel.js
@@ -1,7 +1,11 @@
 // CSS
 
 window.onload = function () {
-    var url = "http://localhost/verkaufszahlen.json";
+    // var url = "http://localhost/verkaufszahlen.json";
+    //var url = "http://localhost/~WebstormProjects/vokabeltainer/training_martin/uebungen5/verkaufszahlen.json";
+    //var url = "http://127.0.0.1/Users/martin/WebstormProjects/vokabeltainer/training_martin/uebungen5/verkaufszahlen.json";
+    var url = "http://localhost/Users/martin/WebstormProjects/vokabeltrainer/Resources/verkaufszahlen.json";
+
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function () {
@@ -15,5 +19,15 @@ window.onload = function () {
 
 function aktualisiereZahlen(responseText) {
     var zahlenDiv = document.getElementById("verkauft");
-    zahlenDiv.innerHTML = responseText;
+    // zahlenDiv.innerHTML = responseText;
+    var verkauft = JSON.parse(responseText);
+
+    for (var i = 0; i < verkauft.length; i++) {
+        var zahl = verkauft[i];
+        var div = document.createElement("div");
+        div.setAttribute("class", "verkaufsZahl");
+        div.innerHTML = zahl.name + ": " + zahl.sales + " Kaugummis verkauft";
+        zahlenDiv.appendChild(div);
+    }
+
 }
